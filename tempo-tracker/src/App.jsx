cf216e8f047efd42b0940413714c763933fb3929
Shannon <sestumpf@gmail.com>
2026-06-18 17:58:23 -0700

    added input form (not hooked up yet)

--- tempo-tracker/src/App.jsx
+++ tempo-tracker/src/App.jsx
@@ -10,6 +10,30 @@ function App() {
   return (
     <>
       <h1>Shannon and Fabián's Fabulous Tempo Tracker App</h1>
+
+      <div className ="form-container">
+      <form>
+        <label htmlFor="songTitle">Enter Song Name</label>
+        <input
+          type="text"
+          id="songTitle"
+          placeholder="Shining Star"
+        />
+
+        <label htmlFor="bpm">Beats Per Minute</label>
+        <input
+          type="number"
+          id="bpm"
+          min="40"
+          max="300"
+          placeholder="120"
+        />
+
+  <button type="submit">
+    Save Song
+  </button>
+</form>
+</div>
     </>
   )
 }
